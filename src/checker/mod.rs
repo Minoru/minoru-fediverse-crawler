11944bc42945c79234ca61851eb7686902e52830
Alexander Batischev <eual.jp@gmail.com>
2021-10-06 19:56:36 +0300

    Replace println with slog

--- src/checker/mod.rs
+++ src/checker/mod.rs
@@ -1,11 +1,24 @@
+use slog::{info, o, Logger};
+use sloggers::{
+    terminal::{Destination, TerminalLoggerBuilder},
+    types::Severity,
+    Build,
+};
 use tokio::runtime::Runtime;
 
 pub fn main(host: String) -> anyhow::Result<()> {
+    let mut builder = TerminalLoggerBuilder::new();
+    builder.level(Severity::Info);
+    builder.destination(Destination::Stderr);
+
+    let logger = builder.build()?;
+
     let rt = Runtime::new()?;
-    rt.block_on(async_main(&host))
+    info!(logger, "Started Tokio runtime");
+    rt.block_on(async_main(logger.new(o!("host" => host.clone())), &host))
 }
 
-async fn async_main(host: &str) -> anyhow::Result<()> {
-    println!("Checking Fediverse instance at {}", host);
+async fn async_main(logger: Logger, _host: &str) -> anyhow::Result<()> {
+    info!(logger, "Started the checker");
     Ok(())
 }
