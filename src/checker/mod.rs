91d29f62d18e08ccf82ca16c9d38b67caf5f0b3e
Alexander Batischev <eual.jp@gmail.com>
2021-10-07 01:46:12 +0300

    Share HTTP client between requests

--- src/checker/mod.rs
+++ src/checker/mod.rs
@@ -1,6 +1,8 @@
 use anyhow::anyhow;
+use reqwest::Client;
 use serde::Deserialize;
 use slog::{error, info, o, Drain, Logger};
+use std::time::Duration;
 use tokio::runtime::Runtime;
 
 pub fn main(host: String) -> anyhow::Result<()> {
@@ -14,14 +16,24 @@ pub fn main(host: String) -> anyhow::Result<()> {
 async fn async_main(logger: &Logger, host: &str) -> anyhow::Result<()> {
     info!(logger, "Started the checker");
 
-    let software = get_software(logger, host).await?;
+    let client = reqwest::ClientBuilder::new()
+        // TODO: set a User Agent with a URL that describes the bot
+        .timeout(Duration::from_secs(30))
+        .build()
+        .map_err(|err| {
+            let msg = format!("Failed to prepare a reqwest client: {}", err);
+            error!(logger, "{}", &msg);
+            anyhow!(msg)
+        })?;
+
+    let software = get_software(logger, &client, host).await?;
     info!(logger, "{} runs {}", host, software);
 
     Ok(())
 }
 
-async fn get_software(logger: &Logger, host: &str) -> anyhow::Result<String> {
-    let nodeinfo = fetch_nodeinfo(logger, host).await?;
+async fn get_software(logger: &Logger, client: &Client, host: &str) -> anyhow::Result<String> {
+    let nodeinfo = fetch_nodeinfo(logger, client, host).await?;
     json::parse(&nodeinfo)
         .map(|obj| obj["software"]["name"].to_string())
         .map_err(|err| {
@@ -45,8 +57,8 @@ struct NodeInfoPointerLink {
     href: String,
 }
 
-async fn fetch_nodeinfo(logger: &Logger, host: &str) -> anyhow::Result<String> {
-    let pointer = fetch_nodeinfo_pointer(logger, host).await?;
+async fn fetch_nodeinfo(logger: &Logger, client: &Client, host: &str) -> anyhow::Result<String> {
+    let pointer = fetch_nodeinfo_pointer(logger, client, host).await?;
     // TODO: add sanitization step that removes any links that point outside of the current host's
     // domain
     let url = pick_highest_supported_nodeinfo_version(&pointer).ok_or_else(|| {
@@ -57,13 +69,24 @@ async fn fetch_nodeinfo(logger: &Logger, host: &str) -> anyhow::Result<String> {
         error!(logger, "{}", &msg);
         anyhow!(msg)
     })?;
-    fetch_nodeinfo_document(logger, &url).await
+    fetch_nodeinfo_document(logger, client, &url).await
 }
 
-async fn fetch_nodeinfo_pointer(logger: &Logger, host: &str) -> anyhow::Result<NodeInfoPointer> {
+async fn fetch_nodeinfo_pointer(
+    logger: &Logger,
+    client: &Client,
+    host: &str,
+) -> anyhow::Result<NodeInfoPointer> {
     let url = format!("https://{}/.well-known/nodeinfo", host);
-    // TODO: set Accept to application/json
-    let response = reqwest::get(&url).await?;
+    let response = client
+        .get(&url)
+        .header(
+            reqwest::header::ACCEPT,
+            reqwest::header::HeaderValue::from_static("application/json"),
+        )
+        .timeout(Duration::from_secs(10))
+        .send()
+        .await?;
     response.error_for_status_ref().map_err(|err| {
         error!(
             logger, "Failed to fetch the well-known NodeInfo document: {}", err;
@@ -96,9 +119,20 @@ fn pick_highest_supported_nodeinfo_version(pointer: &NodeInfoPointer) -> Option<
         .map(|result| result.1.href.clone())
 }
 
-async fn fetch_nodeinfo_document(logger: &Logger, url: &str) -> anyhow::Result<String> {
-    // TODO: set Accept to application/json
-    let response = reqwest::get(url).await?;
+async fn fetch_nodeinfo_document(
+    logger: &Logger,
+    client: &Client,
+    url: &str,
+) -> anyhow::Result<String> {
+    let response = client
+        .get(url)
+        .header(
+            reqwest::header::ACCEPT,
+            reqwest::header::HeaderValue::from_static("application/json"),
+        )
+        .timeout(Duration::from_secs(10))
+        .send()
+        .await?;
     response.error_for_status_ref().map_err(|err| {
         error!(
             logger, "Failed to fetch NodeInfo: {}", err;
