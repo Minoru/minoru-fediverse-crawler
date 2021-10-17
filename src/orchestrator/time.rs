7148e29323a2ca9993c1df56fa8df4109e28a277
Alexander Batischev <eual.jp@gmail.com>
2021-10-17 22:05:56 +0300

    Simplify time randomization
    Percent are hard to interpret (quick, what's 10% of a day?). Replace
    that with 2 hours.

--- src/orchestrator/time.rs
+++ src/orchestrator/time.rs
@@ -2,12 +2,15 @@
 use anyhow::anyhow;
 use chrono::prelude::*;
 
-/// Random datetime about a day away from now (±10%).
+/// The number of seconds in two hours.
+const TWO_HOURS_SECS: i64 = 2 * 60 * 60;
+
+/// Random datetime about a day away from now (±2 hours).
 pub fn rand_datetime_daily() -> anyhow::Result<DateTime<Utc>> {
     use chrono::Duration;
 
-    const OFFSET: i64 = 24 * 60 * 60 / 10; // 10% of the day
-    let offset = Duration::days(1) + Duration::seconds(fastrand::i64(-OFFSET..OFFSET));
+    let offset =
+        Duration::days(1) + Duration::seconds(fastrand::i64(-TWO_HOURS_SECS..TWO_HOURS_SECS));
     Utc::now().checked_add_signed(offset).ok_or_else(|| {
         anyhow!(
             "Failed to add {} to the current datetime, as it will lead to overflow",
