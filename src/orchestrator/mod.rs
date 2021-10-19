9649fdc0adf6aaa8c9dc0f7014cf23228739a796
Alexander Batischev <eual.jp@gmail.com>
2021-10-19 23:21:27 +0300

    Do not busy-wait for opportunity to start a new thread

--- src/orchestrator/mod.rs
+++ src/orchestrator/mod.rs
@@ -16,40 +16,38 @@ pub fn main(logger: Logger) -> anyhow::Result<()> {
     db::disengage_previous_checks(&conn)?;
 
     loop {
-        if db::outstanding_checks_count(&conn)? < 2 * CHECKERS_COUNT.load(Ordering::SeqCst) {
-            continue;
-        }
-
-        CHECKERS_COUNT.fetch_add(1, Ordering::SeqCst);
-        info!(
-            logger,
-            "Spawning a new checker for a total of {}",
-            CHECKERS_COUNT.load(Ordering::SeqCst)
-        );
-        println!(
-            "{} Spawning a new checker for a total of {}",
-            ">".repeat(35),
-            CHECKERS_COUNT.load(Ordering::SeqCst)
-        );
-
-        let logger = logger.clone();
-        spawn(move || {
-            if let Err(e) = instance_checker::run(logger.clone()) {
-                error!(logger, "Checker error: {}", e);
-            }
-
-            CHECKERS_COUNT.fetch_sub(1, Ordering::SeqCst);
+        if db::outstanding_checks_count(&conn)? > 2 * CHECKERS_COUNT.load(Ordering::SeqCst) {
+            CHECKERS_COUNT.fetch_add(1, Ordering::SeqCst);
             info!(
                 logger,
-                "Checker finished, {} remain",
+                "Spawning a new checker for a total of {}",
                 CHECKERS_COUNT.load(Ordering::SeqCst)
             );
             println!(
-                "{} Checker finished, {} remain",
-                "<".repeat(35),
+                "{} Spawning a new checker for a total of {}",
+                ">".repeat(35),
                 CHECKERS_COUNT.load(Ordering::SeqCst)
             );
-        });
+
+            let logger = logger.clone();
+            spawn(move || {
+                if let Err(e) = instance_checker::run(logger.clone()) {
+                    error!(logger, "Checker error: {}", e);
+                }
+
+                CHECKERS_COUNT.fetch_sub(1, Ordering::SeqCst);
+                info!(
+                    logger,
+                    "Checker finished, {} remain",
+                    CHECKERS_COUNT.load(Ordering::SeqCst)
+                );
+                println!(
+                    "{} Checker finished, {} remain",
+                    "<".repeat(35),
+                    CHECKERS_COUNT.load(Ordering::SeqCst)
+                );
+            });
+        }
 
         sleep(Duration::from_secs(10));
     }
