9d0aaae7ac3a19397c85acf5871f90cac8aa5692
Alexander Batischev <eual.jp@gmail.com>
2021-11-04 02:25:14 +0300

    Wrap reqwest::Client in our own struct, HttpClient
    For now, this lets us scrape some boilerplate, like setting the Accept
    header. But in the future, we'll be able to add more features on top,
    like automatically checking if robots.txt allows us to access a certain
    URL.

--- src/checker/mod.rs
+++ src/checker/mod.rs
@@ -1,9 +1,9 @@
-use crate::ipc;
-use anyhow::{anyhow, Context};
-use reqwest::Client;
+mod http_client;
+
+use crate::{checker::http_client::HttpClient, ipc};
+use anyhow::anyhow;
 use serde::Deserialize;
 use slog::{error, info, o, Logger};
-use std::time::Duration;
 use tokio::runtime::Runtime;
 use url::{Host, Url};
 
@@ -18,7 +18,7 @@ pub fn main(logger: Logger, host: Host) -> anyhow::Result<()> {
 async fn async_main(logger: &Logger, host: &Host) -> anyhow::Result<()> {
     info!(logger, "Started the checker");
 
-    let client = prepare_reqwest_client(logger)?;
+    let client = HttpClient::new(logger)?;
 
     let software = get_software(logger, &client, host).await?;
     info!(logger, "{} runs {}", host, software);
@@ -38,37 +38,7 @@ async fn async_main(logger: &Logger, host: &Host) -> anyhow::Result<()> {
     Ok(())
 }
 
-fn prepare_reqwest_client(logger: &Logger) -> anyhow::Result<Client> {
-    // Accept no more than 5 redirects, and only allow redirects to current domain and its
-    // subdomains
-    let redirect_policy = {
-        let logger = logger.clone();
-        reqwest::redirect::Policy::custom(move |attempt| {
-            if attempt.previous().len() > 5 {
-                error!(logger, "Too many redirects: {:?}", attempt.previous());
-                attempt.error("too many redirects")
-            } else if !is_same_origin(attempt.url(), &attempt.previous()[0]) {
-                error!(
-                    logger,
-                    "Redirect points to {} which is of different origin than {}",
-                    attempt.url(),
-                    &attempt.previous()[0]
-                );
-                attempt.error("redirected outside of current origin")
-            } else {
-                attempt.follow()
-            }
-        })
-    };
-    reqwest::ClientBuilder::new()
-        // TODO: set a User Agent with a URL that describes the bot
-        .redirect(redirect_policy)
-        .timeout(Duration::from_secs(30))
-        .build()
-        .context("Failed to prepare a reqwest client")
-}
-
-async fn get_software(logger: &Logger, client: &Client, host: &Host) -> anyhow::Result<String> {
+async fn get_software(logger: &Logger, client: &HttpClient, host: &Host) -> anyhow::Result<String> {
     let nodeinfo = fetch_nodeinfo(logger, client, host).await?;
     json::parse(&nodeinfo)
         .map(|obj| obj["software"]["name"].to_string())
@@ -93,7 +63,11 @@ struct NodeInfoPointerLink {
     href: String,
 }
 
-async fn fetch_nodeinfo(logger: &Logger, client: &Client, host: &Host) -> anyhow::Result<String> {
+async fn fetch_nodeinfo(
+    logger: &Logger,
+    client: &HttpClient,
+    host: &Host,
+) -> anyhow::Result<String> {
     let pointer = fetch_nodeinfo_pointer(logger, client, host).await?;
     // TODO: add sanitization step that removes any links that point outside of the current host's
     // domain
@@ -103,19 +77,11 @@ async fn fetch_nodeinfo(logger: &Logger, client: &Client, host: &Host) -> anyhow
 
 async fn fetch_nodeinfo_pointer(
     logger: &Logger,
-    client: &Client,
+    client: &HttpClient,
     host: &Host,
 ) -> anyhow::Result<NodeInfoPointer> {
     let url = format!("https://{}/.well-known/nodeinfo", host);
-    let response = client
-        .get(&url)
-        .header(
-            reqwest::header::ACCEPT,
-            reqwest::header::HeaderValue::from_static("application/json"),
-        )
-        .timeout(Duration::from_secs(10))
-        .send()
-        .await?;
+    let response = client.get(&url).await?;
     response.error_for_status_ref().map_err(|err| {
         error!(
             logger, "Failed to fetch the well-known NodeInfo document: {}", err;
@@ -157,18 +123,10 @@ fn pick_highest_supported_nodeinfo_version(pointer: &NodeInfoPointer) -> anyhow:
 
 async fn fetch_nodeinfo_document(
     logger: &Logger,
-    client: &Client,
+    client: &HttpClient,
     url: &Url,
 ) -> anyhow::Result<String> {
-    let response = client
-        .get(url.clone())
-        .header(
-            reqwest::header::ACCEPT,
-            reqwest::header::HeaderValue::from_static("application/json"),
-        )
-        .timeout(Duration::from_secs(10))
-        .send()
-        .await?;
+    let response = client.get(url.clone()).await?;
     response.error_for_status_ref().map_err(|err| {
         error!(
             logger, "Failed to fetch NodeInfo: {}", err;
@@ -180,36 +138,9 @@ async fn fetch_nodeinfo_document(
     Ok(response.text().await?)
 }
 
-/// Returns `true` if all of the following holds:
-/// - the URLs have the same schema
-/// - the URLs use the same domain, or one domain is a sub-domain of another
-/// - the URLs use the same port (if any; port can be implied by the schema)
-fn is_same_origin(lhs: &Url, rhs: &Url) -> bool {
-    use url::{Host::*, Origin::*};
-
-    match (lhs.origin(), rhs.origin()) {
-        (Opaque(lhs), Opaque(rhs)) => lhs == rhs,
-        (Opaque(_), _) => false,
-        (_, Opaque(_)) => false,
-        (Tuple(lhs_schema, lhs_host, lhs_port), Tuple(rhs_schema, rhs_host, rhs_port)) => {
-            let same_host = match (lhs_host, rhs_host) {
-                (Domain(lhs), Domain(rhs)) => lhs
-                    .rsplit('.')
-                    .zip(rhs.rsplit('.'))
-                    .all(|(lhs, rhs)| lhs == rhs),
-                (Ipv4(lhs), Ipv4(rhs)) => lhs == rhs,
-                (Ipv6(lhs), Ipv6(rhs)) => lhs == rhs,
-                _ => false,
-            };
-
-            lhs_schema == rhs_schema && same_host && lhs_port == rhs_port
-        }
-    }
-}
-
 async fn get_peers(
     logger: &Logger,
-    client: &Client,
+    client: &HttpClient,
     host: &Host,
     software: &str,
 ) -> anyhow::Result<Vec<Host>> {
@@ -223,19 +154,11 @@ async fn get_peers(
 
 async fn get_peers_mastodonish(
     logger: &Logger,
-    client: &Client,
+    client: &HttpClient,
     host: &Host,
 ) -> anyhow::Result<Vec<Host>> {
     let url = format!("https://{}/api/v1/instance/peers", host);
-    let response = client
-        .get(&url)
-        .header(
-            reqwest::header::ACCEPT,
-            reqwest::header::HeaderValue::from_static("application/json"),
-        )
-        .timeout(Duration::from_secs(10))
-        .send()
-        .await?;
+    let response = client.get(&url).await?;
     response.error_for_status_ref().map_err(|err| {
         error!(
             logger, "Failed to fetch Mastodon-ish peers: {}", err;
@@ -314,26 +237,5 @@ mod test {
             .unwrap(),
             Url::parse("http://example.org/highest is the first").unwrap()
         );
-    }
-
-    #[test]
-    fn test_origin() {
-        let http_example_com = Url::parse("http://example.com").unwrap();
-        let https_example_com = Url::parse("https://example.com").unwrap();
-        let https_foo_example_com = Url::parse("https://foo.example.com").unwrap();
-        let https_example_com_443 = Url::parse("https://example.com:443").unwrap();
-        let https_example_com_444 = Url::parse("https://example.com:444").unwrap();
-        let https_example_org = Url::parse("https://example.org").unwrap();
-
-        assert!(!is_same_origin(&http_example_com, &https_example_com));
-        assert!(!is_same_origin(&https_example_com, &http_example_com));
-        assert!(!is_same_origin(&https_example_com, &https_example_org));
-        assert!(!is_same_origin(&https_example_com, &https_example_com_444));
-
-        assert!(is_same_origin(&https_example_com, &https_example_com));
-        assert!(is_same_origin(&https_example_com, &https_foo_example_com));
-        assert!(is_same_origin(&https_foo_example_com, &https_example_com));
-
-        assert!(is_same_origin(&https_example_com, &https_example_com_443));
     }
 }
